873b77d9fa25b6ee9b033a1ade5187a5c0afc3c4
Roberto Pacheco <robertopvc@gmail.com>
2024-02-23 15:26:30 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -38,25 +38,25 @@ lr_scheduler='cosineannealinglr'
 lr_warmup_epochs=5
 lr_warmup_method='linear' 
 lr_warmup_decay=0.01
-norm_weight_decay=0.0,
-label_smoothing=0.1, 
-mixup_alpha=0.2, 
-cutmix_alpha=1.0, 
-auto_augment='ta_wide', 
-random_erase=0.1, 
+norm_weight_decay=0.0
+label_smoothing=0.1 
+mixup_alpha=0.2 
+cutmix_alpha=1.0 
+auto_augment='ta_wide'
+random_erase=0.1
   
-ra_sampler=True,
-ra_reps=4,
+ra_sampler=True
+ra_reps=4
 ra_magnitude = 9
 
 # EMA configuration
-model_ema=True, 
-model_ema_steps=32, 
+model_ema=True 
+model_ema_steps=32 
 model_ema_decay=0.99998
 
 # Resizing
 interpolation='bilinear'
-val_resize_size=232, 
-val_crop_size=224, 
+val_resize_size=232 
+val_crop_size=224
 train_crop_size=176
 hflip_prob = 0.5
