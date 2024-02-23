381b57b5f4667783bf432c3a8acd51610087dd25
Roberto Pacheco <robertopvc@gmail.com>
2024-02-23 10:44:10 -0300

    Add files via upload

--- ee_dnn.py
+++ ee_dnn.py
@@ -10,7 +10,7 @@ import pandas as pd
 #import torchvision.models as models
 from pthflops import count_ops
 from torch import Tensor
-import torchvision.prototype.models as models
+from torchvision.prototype.models import mobilenet_v2
 
 class EarlyExitBlock(nn.Module):
   """
@@ -224,7 +224,7 @@ class Early_Exit_DNN(nn.Module):
     
 		# Loads the backbone model. In other words, Mobilenet architecture provided by Pytorch.
 		#backbone_model = models.mobilenet_v2(self.pretrained, weights='DEFAULT').to(self.device)
-		backbone_model = models.mobilenet_v2(self.pretrained, weights=MobileNet_V2_Weights.DEFAULT).to(self.device)
+		backbone_model = mobilenet_v2(self.pretrained, weights=MobileNet_V2_Weights.DEFAULT).to(self.device)
 
 		# It verifies if the number of early exits provided is greater than a number of layers in the backbone DNN model.
 		self.verifies_nr_exits(backbone_model.features)
