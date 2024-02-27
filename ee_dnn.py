14985a9b70767706a6f3f8756f086a39f03964e6
Roberto Pacheco <robertopvc@gmail.com>
2024-02-27 12:33:16 -0300

    Add files via upload

--- ee_dnn.py
+++ ee_dnn.py
@@ -225,8 +225,8 @@ class Early_Exit_DNN(nn.Module):
 		last_channel = 1280
     
 		# Loads the backbone model. In other words, Mobilenet architecture provided by Pytorch.
-		#backbone_model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT).to(self.device)
-		backbone_model = PM.mobilenet(self.pretrained).to(self.device)
+		backbone_model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT).to(self.device)
+		#backbone_model = PM.mobilenet(self.pretrained).to(self.device)
 
 		# It verifies if the number of early exits provided is greater than a number of layers in the backbone DNN model.
 		self.verifies_nr_exits(backbone_model.features)
