bc3ddb3818f120ec6dd041cf7f147bda7b70187c
Roberto Pacheco <robertopvc@gmail.com>
2024-02-28 18:56:55 -0300

    Add files via upload

--- extract_inference_data.py
+++ extract_inference_data.py
@@ -72,7 +72,7 @@ def main(args):
 	inf_data_path = os.path.join(inf_data_dir_path, "inf_data_ee_%s_%s_branches_%s_id_%s.csv"%(args.model_name, 
 		args.n_branches, args.loss_weights_type, args.model_id))
 	
-	ee_model = load_eednn_model(args, n_classes, model_path, device)
+	ee_model = utils.load_eednn_model(args, n_classes, model_path, device)
 
 	distortion_level_list = config.distortion_level_dict[args.distortion_type]
 
