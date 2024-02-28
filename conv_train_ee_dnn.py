babf5990082ac416867a0c94aede70586d174832
Roberto Pacheco <robertopvc@gmail.com>
2024-02-28 10:25:09 -0300

    Add files via upload

--- conv_train_ee_dnn.py
+++ conv_train_ee_dnn.py
@@ -33,9 +33,10 @@ def trainEEDNNs(model, train_loader, optimizer, criterion, n_exits, epoch, devic
 	for (data, target) in tqdm(train_loader):
 		data, target = data.to(device), target.to(device)
 
-		output_list, conf_list, class_list, _, _ = model.forwardTraining(data)
-		optimizer.zero_grad()
+		with torch.cuda.amp.autocast(enabled=True):
+			output_list, conf_list, class_list, _, _ = model.forwardTraining(data)
 
+		optimizer.zero_grad()
 
 		model_loss, ee_loss, model_acc, ee_acc = compute_metrics(criterion, output_list, conf_list, class_list, target, loss_weights)
 
@@ -43,8 +44,14 @@ def trainEEDNNs(model, train_loader, optimizer, criterion, n_exits, epoch, devic
 
 		model_acc_list.append(model_acc), ee_acc_list.append(ee_acc)
 
-		model_loss.backward()
-		optimizer.step()
+
+		scaler.scale(model_loss).backward()
+
+		scaler.step(optimizer)
+		scaler.update()
+
+		#model_loss.backward()
+		#optimizer.step()
 
 		# clear variables
 		del data, target, output_list, conf_list, class_list
@@ -148,15 +155,36 @@ def main(args):
 
 	lr = [1.5e-4, 0.01]
 
-	criterion = nn.CrossEntropyLoss()
+	#criterion = nn.CrossEntropyLoss()
+	criterion = nn.CrossEntropyLoss(label_smoothing=config.label_smoothing)
 
-	optimizer = optim.Adam([{'params': ee_model.stages.parameters(), 'lr': lr[0]}, 
-		{'params': ee_model.exits.parameters(), 'lr': lr[1]},
-		{'params': ee_model.classifier.parameters(), 'lr': lr[0]}], weight_decay=args.weight_decay)
+	#optimizer = optim.Adam([{'params': ee_model.stages.parameters(), 'lr': lr[0]}, 
+	#	{'params': ee_model.exits.parameters(), 'lr': lr[1]},
+	#	{'params': ee_model.classifier.parameters(), 'lr': lr[0]}], weight_decay=args.weight_decay)
+
+	optimizer = optim.SGD([{'params': ee_model.stages.parameters(), 'lr': config.lr}, 
+		{'params': ee_model.exits.parameters(), 'lr': config.lr},
+		{'params': ee_model.classifier.parameters(), 'lr': config.lr}], 
+		momentum=config.momentum, weight_decay=args.weight_decay)
+
 
 	#scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, eta_min=0, last_epoch=-1, verbose=True)
-	scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
+	#scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
+	#	T_max=config.max_epochs - config.lr_warmup_epochs, eta_min=config.lr_min)
+
+
+	scaler = torch.cuda.amp.GradScaler()
+
+	main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
 		T_max=config.max_epochs - config.lr_warmup_epochs, eta_min=config.lr_min)
+
+
+	warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, 
+		start_factor=config.lr_warmup_decay, total_iters=config.lr_warmup_epochs)
+
+	lr_scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, 
+		schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[config.lr_warmup_epochs])
+
 
 	n_exits = args.n_branches + 1
 
@@ -164,12 +192,14 @@ def main(args):
 	best_val_loss = np.inf
 	df_history = pd.DataFrame()
 
-	while (count_patience < args.max_patience):
+	#while (count_patience < args.max_patience):
+	while (epoch < config.max_epochs):
+
 		epoch += 1
 
-		train_result = trainEEDNNs(ee_model, train_loader, optimizer, criterion, n_exits, epoch, device, loss_weights)
+		train_result = trainEEDNNs(ee_model, train_loader, optimizer, criterion, n_exits, epoch, device, loss_weights, scaler)
 		val_result = evalEEDNNs(ee_model, val_loader, criterion, n_exits, epoch, device, loss_weights)
-		scheduler.step()
+		lr_scheduler.step()
 
 		current_result.update(train_result), current_result.update(val_result)
 		#df_history = df_history.append(pd.Series(current_result), ignore_index=True)
