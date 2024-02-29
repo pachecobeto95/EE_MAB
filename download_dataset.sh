1759e0d3cd7f9daf30c9f8c0b7a12affce86b402
Roberto Pacheco <robertopvc@gmail.com>
2024-02-28 21:41:12 -0300

    Add files via upload

--- download_dataset.sh
+++ download_dataset.sh
@@ -2,6 +2,7 @@
 
 #Download caltech-256 dataset with pristine images
 wget https://data.caltech.edu/records/nyy15-4j048/files/256_ObjectCategories.tar?download=1
+#Install the required libraries
 sudo apt install xz-utils
 tar -xvf 256_ObjectCategories.tar?download=1
 rm 256_ObjectCategories.tar?download=1
