ef19266aba4581ecf9534570bf58aa5717f9e153
Roberto Pacheco <robertopvc@gmail.com>
2024-03-07 13:45:13 -0300

    Add files via upload

--- run_exp_ucb.sh
+++ run_exp_ucb.sh
@@ -2,7 +2,11 @@
 
 nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way ucb --n_iter 100000 &
 nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way random --n_iter 100000 &
-nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --n_iter 100000 &
-#nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.6 --n_iter 100000 &
+nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.6 --n_iter 100000 &
 nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.7 --n_iter 100000 &
 nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.9 --n_iter 100000 &
+
+
+#nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way ucb --n_iter 100000 &
+
+#nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function alpha-fairness --arm_selection_way ucb --n_iter 100000 &
