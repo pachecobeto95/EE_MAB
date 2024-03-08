1bb9c58cce764f9f93a7484907e39e513b8f3a64
Roberto Pacheco <robertopvc@gmail.com>
2024-03-08 12:56:18 -0300

    Add files via upload

--- run_exp_ucb.sh
+++ run_exp_ucb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way ucb --n_iter 100000 &
-nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way random --n_iter 100000 &
-nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.6 --n_iter 100000 &
-nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.7 --n_iter 100000 &
-nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic --arm_selection_way fixed_threshold --fixed_threshold 0.9 --n_iter 100000 &
+nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way ucb --n_iter 100000 &
+nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way random --n_iter 100000 &
+nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way fixed_threshold --fixed_threshold 0.6 --n_iter 100000 &
+nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way fixed_threshold --fixed_threshold 0.7 --n_iter 100000 &
+nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way fixed_threshold --fixed_threshold 0.9 --n_iter 100000 &
 
 
 #nohup python3 exp_ucb.py --n_branches 1 --n_rounds 5 --model_id 3 --loss_weights_type equal --distortion_type blur --reward_function adaee_basic2 --arm_selection_way ucb --n_iter 100000 &
